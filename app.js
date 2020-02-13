995bda9973318563b469c211f18ea3654eb64365
Latypov Mansur <latypov199619@gmail.com>
2020-02-13 20:39:13 +0500

    try

---
+++ app.js
@@ -0,0 +1,8 @@
+document,addEventListener('DOMContentLoaded', () =>{
+
+	let div = document.createElement("div"),
+	container = document.querySelector(".container");
+	div.className = "red";
+	div.innerHTML = "Привет, епта!";
+	body.append(div);
+})
