d8f550855af323d4d332c36793a039c227c39726
Latypov Mansur <latypov199619@gmail.com>
2020-02-14 10:20:37 +0500

    добавлена elemCreate функция

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-document,addEventListener('DOMContentLoaded', () =>{
+let div = document.createElement("div"),
+  container = document.querySelector(".container");
+div.className = "red";
+div.innerHTML = "Привет, епта!";
+// body.append(div);
 
-	let div = document.createElement("div"),
-	container = document.querySelector(".container");
-	div.className = "red";
-	div.innerHTML = "Привет, епта!";
-	body.append(div);
-})
+function divCreate(classList, id) {
+  let elem = document.createElement("div");
+  if (typeof classList === "object")
+    for (let i = 0; i < classList.length; i++) {
+      elem.classList.add(classList[i]);
+		}
+	else elem.classList.add(classList)
+	elem.id = id
+	return elem
+}
+
+
+
+console.log(divCreate(['класс1', 'класс2'], 'id'));
