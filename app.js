5de91e7324f5495fb07b3d13d9e05093e8654c36
Latypov Mansur <latypov199619@gmail.com>
2020-02-14 16:16:21 +0500

    for transfer

--- app.js
+++ app.js
@@ -11,28 +11,39 @@ function createElem(tag, classList, attrs, innerHTML = "") {
   return elem;
 }
 
-function createHTMLBranch(array) {
+function createHTMLBranch(array, initialElem) {
   array.forEach((node, i) => {
-    if (Array.isArray(array[i + 1])) {
-      createHTMLBranch(array[i + 1]);
+    if (node instanceof Object && !(array[i + 1] instanceof Array))
+      console.log("dom");
+		initialElem.append(node);
+		debugger
+    if (node instanceof Array) {
+      let innerBranch = node;
+      console.log(innerBranch);
+
+      if (array[i-1] !== undefined)
+			createHTMLBranch(innerBranch, array[i-1])
+			// debugger;
     }
-    else array[i].append(array[i + 1]);
   });
 }
 
-createHTMLBranch([
-  container,
+createHTMLBranch(
   [
+    createElem("div", "start-page", { id: "start-page" }),
     [
-      createElem("div", "new-class", { id: "idishnik" }),
       [
-        createElem("div", "title", { id: "title" }),
-        createElem("div", "sub-title", { id: "sub-title" })
+        createElem("img", "start-page-img", {
+          src:
+            "https://res.cloudinary.com/hgwipn3sa/image/upload/dpr_1.0,f_auto,h_650/sgp59amwpq4agwdvk5z6.jpg"
+        }),
+        createElem("div", "new-class", { id: "idishnik" }),
+        [
+          createElem("div", "title", { id: "title" }),
+          createElem("div", "sub-title", { id: "sub-title" })
+        ]
       ]
-    ],
-    createElem("img", "start-page-img", {
-      src:
-        "https://res.cloudinary.com/hgwipn3sa/image/upload/dpr_1.0,f_auto,h_650/sgp59amwpq4agwdvk5z6.jpg"
-    })
-  ]
-]);
+    ]
+  ],
+  container
+);
